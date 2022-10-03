83a9067d7c450868ea01597ab1d65e9b8f79e406
Eugénie <eugenie.philippe@hotmail.fr>
2022-10-03 18:50:53 +0200

    Error added

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import {createRoot} from "react-dom/client"
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals'
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
 import Homepage from "./pages/Homepage/Homepage" 
 import About from "./pages/About/About" 
+import Error from "./pages/Error/Error"
+import LocationDetail from "./pages/LocationDetail/LocationDetail" 
 
 const rootElement = document.getElementById('root')
   if (rootElement != null){
     const root = createRoot(rootElement)
   root.render(//will render each component that will build the page as itself
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}/>
-      </Routes>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="hotels/:locationID" element={<LocationDetail />}/>
+        <Route path='/*' element={<Error />} />
       </Routes>
-      <Routes>
-        <Route path="/about" element={<About />}/>
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Footer />}/>
-      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
