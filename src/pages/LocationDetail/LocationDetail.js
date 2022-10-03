25b4af0f123ef97d77a5b58dfd3610ac4ee8596d
Eugénie <eugenie.philippe@hotmail.fr>
2022-10-03 18:50:46 +0200

    Location Edit

--- src/pages/LocationDetail/LocationDetail.js
+++ src/pages/LocationDetail/LocationDetail.js
@@ -1,1 +1,50 @@
 import React from 'react'
+import { useParams } from "react-router-dom"
+
+import "./LocationDetail.css"
+import Gallery from "../../components/gallery/gallery"
+import TagsGenerator from "../../components/tagsGenerator/tagsGenerator"
+import Collapse from "../../components/collapse/collapse"
+import locationList  from "../../data/hotels.json"
+import StarsRating from "../../components/starsRating/starsRating"
+
+const LocationDetail = () => {
+
+    const { locationID } = useParams()
+    const locationSelected = locationList.find((location) => location.id === locationID)
+    const { title, location, equipments, description, rating } = locationSelected
+
+    return (
+            <div className="location">
+              <Gallery pictures={locationSelected?.pictures}/>
+              <div className="locationChecked">
+                <div className="locationDetails">
+                  <h1 className="locationName">{title}</h1>
+                  <p className="locationPlace">{location}</p>
+                  <div className="locationTags">
+                    {locationSelected.tags.map((tag, index) => (
+                      <TagsGenerator key={index} getTag={tag} />
+                    ))}
+                  </div>
+                </div>
+                <div className="locationLandlord">
+                  <h3>{locationSelected?.host.name}</h3>
+                  <img className='landlordPicture' src={locationSelected?.host.picture} alt={locationSelected?.host.name}/>
+                </div>
+                <div className='locationRating'>
+                  <StarsRating rating={rating} />
+                </div>
+              </div>
+              <div className="equip-dropdowns">
+                <Collapse label="Description" >
+                <p>{description}</p> 
+                </Collapse> 
+                <Collapse label="Equipement"> 
+                    <span className='tools'>{equipments}</span> 
+                </Collapse>
+              </div>
+            </div>
+          )
+        }
+
+export default LocationDetail
